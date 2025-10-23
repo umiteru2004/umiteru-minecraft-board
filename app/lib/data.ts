139a7a2103f04053c4f17bc10e9c5b2bba0a3039
umiteru2004 <umiteru2004@gmail.com>
2025-10-23 13:48:09 +0900

    feat: 全ての投稿を取得する関数を追加しました。

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,3 +3,22 @@ import { Client } from "@notionhq/client";
 const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
+
+export async function fetchPosts() {
+  try {
+    const dataSourceId = "22cfcb08-f46a-8089-a486-000b840004ea";
+    const response = await notion.dataSources.query({
+      data_source_id: dataSourceId,
+      sorts: [
+        {
+          property: "last_updated",
+          direction: "descending",
+        },
+      ],
+    });
+
+    return response.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
